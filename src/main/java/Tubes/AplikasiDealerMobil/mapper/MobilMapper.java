6327375c356b1537284652fdd07c0cd457c191ec
dickysinabang <dickysinabang@gmail.com>
2025-01-03 21:53:39 +0700

    Update MobilMapper.java

--- src/main/java/Tubes/AplikasiDealerMobil/mapper/MobilMapper.java
+++ src/main/java/Tubes/AplikasiDealerMobil/mapper/MobilMapper.java
@@ -4,4 +4,34 @@ import Tubes.AplikasiDealerMobil.dto.MobilDto;
 import Tubes.AplikasiDealerMobil.model.Mobil;
 
 public class MobilMapper {
+    public static MobilDto mapToMobilDto(Mobil mobil) {
+        return new MobilDto(
+                mobil.getId(),
+                mobil.getNamaMobil(),
+                mobil.getJenisMobil(),
+                mobil.getTahun(),
+                mobil.getStatus(),
+                mobil.getNomorRangka(),
+                mobil.getNomorMesin(),
+                mobil.getNomorPolisi(),
+                mobil.getKapasitasMesin(),
+                mobil.getWarna(),
+                mobil.getPabrikan()
+        );
+    }
+
+    public static Mobil mapToMobil(MobilDto dto) {
+        Mobil mobil = new Mobil();
+        mobil.setNamaMobil(dto.getNamaMobil());
+        mobil.setJenisMobil(dto.getJenisMobil());
+        mobil.setTahun(dto.getTahun());
+        mobil.setStatus(dto.getStatus());
+        mobil.setNomorRangka(dto.getNomorRangka());
+        mobil.setNomorMesin(dto.getNomorMesin());
+        mobil.setNomorPolisi(dto.getNomorPolisi());
+        mobil.setKapasitasMesin(dto.getKapasitasMesin());
+        mobil.setWarna(dto.getWarna());
+        mobil.setPabrikan(dto.getPabrikan());
+        return mobil;
+    }
 }
