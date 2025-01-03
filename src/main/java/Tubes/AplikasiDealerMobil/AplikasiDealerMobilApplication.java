81fb5fef8ad9454c4f949b2afa37e38d07a8c59e
Muhammad Aulia <muauli@student.telkomuniversity.ac.id>
2025-01-03 18:27:39 +0700

    Start

---
+++ src/main/java/Tubes/AplikasiDealerMobil/AplikasiDealerMobilApplication.java
@@ -0,0 +1,13 @@
+package Tubes.AplikasiDealerMobil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiDealerMobilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiDealerMobilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tubes/AplikasiDealerMobil/AplikasiDealerMobilApplicationTests.java
@@ -0,0 +1,13 @@
+package Tubes.AplikasiDealerMobil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiDealerMobilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
