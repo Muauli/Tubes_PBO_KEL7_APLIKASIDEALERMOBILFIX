fc5979817047b3a808a27b38b63ffffc71400138
Haikal Atmajati <haikalafi666@gmail.com>
2025-01-03 22:23:22 +0700

    Update UserService.java

--- src/main/java/Tubes/AplikasiDealerMobil/service/UserService.java
+++ src/main/java/Tubes/AplikasiDealerMobil/service/UserService.java
@@ -5,4 +5,9 @@ import Tubes.AplikasiDealerMobil.dto.UserDto;
 import java.util.List;
 
 public interface UserService {
+    UserDto createUser(UserDto userDto);
+
+    String getCurrentUserRole(String username);
+
+    boolean authenticateUser(String username, String password);
 }
