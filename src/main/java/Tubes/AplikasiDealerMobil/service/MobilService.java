07ff3a651047035c96ccd244830016a3a8c2deef
Rizki Kasim <123645689+rizkikasim@users.noreply.github.com>
2025-01-04 07:29:37 +0700

    Update MobilService.java

--- src/main/java/Tubes/AplikasiDealerMobil/service/MobilService.java
+++ src/main/java/Tubes/AplikasiDealerMobil/service/MobilService.java
@@ -5,4 +5,10 @@ import Tubes.AplikasiDealerMobil.dto.MobilDto;
 import java.util.List;
 
 public interface MobilService {
+    MobilDto saveMobil(MobilDto mobilDto);
+    List<MobilDto> getAllMobils();
+    MobilDto getMobilById(Long MobilId);
+    void deleteMobilById(Long MobilId);
+    MobilDto updateMobil(MobilDto mobilDto);
+    List<MobilDto> searchMobil(String search);
 }
